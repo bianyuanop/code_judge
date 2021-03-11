316f871027c43cefb48efff69b844ba9cc632b78
bianyuanop <m13320507359@163.com>
2021-03-11 10:28:45 +0800

    fix tab error

--- lib/runner.py
+++ lib/runner.py
@@ -4,100 +4,100 @@ from termcolor import colored
 
 
 Errors = {
-    0: "RE",
-    1: "TLE",
-    2: "WA",
-    3: "",
+	0: "RE",
+	1: "TLE",
+	2: "WA",
+	3: "",
 }
 class Runner:
-    '''
-    a code runner 
-    '''
-    
-    def __init__(self, lang, code, limit):
-        '''
-        limit parse
-            - time
-            - mem
-        '''
-        with open("temp.cpp", 'w') as f:
-            f.write(code)
+	'''
+	a code runner 
+	'''
+	
+	def __init__(self, lang, code, limit):
+		'''
+		limit parse
+			- time
+			- mem
+		'''
+		with open("temp.cpp", 'w') as f:
+			f.write(code)
 
-        self.filename = 'temp.cpp'
-        self.lang = lang
-        self.timeLimit = limit['time']
-        self.memLimit = limit['mem']
+		self.filename = 'temp.cpp'
+		self.lang = lang
+		self.timeLimit = limit['time']
+		self.memLimit = limit['mem']
 
-    def run(self):
-        '''
-        using ulimit for mem and time limit
-        and using subprocess for error code, stdout, error message check
-        example:
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            errcode = p.wait()
-            retval = p.stdout.read()
-            errmess = p.stderr.read()
-        '''
-        if self.lang.lower() == 'python':
-            cmd = ['python', self.filename]
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            err_code = p.wait()
-            retval = p.stdout.read().decode('utf8')
-            errmess = p.stderr.read().decode('utf8')
+	def run(self):
+		'''
+		using ulimit for mem and time limit
+		and using subprocess for error code, stdout, error message check
+		example:
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			errcode = p.wait()
+			retval = p.stdout.read()
+			errmess = p.stderr.read()
+		'''
+		if self.lang.lower() == 'python':
+			cmd = ['python', self.filename]
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			err_code = p.wait()
+			retval = p.stdout.read().decode('utf8')
+			errmess = p.stderr.read().decode('utf8')
 
-            if err_code:
-                print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
-            else:
-                print(colored('[INFO]', 'green'), "running finished ", retval)
-                return retval
+			if err_code:
+				print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
+			else:
+				print(colored('[INFO]', 'green'), "running finished ", retval)
+				return retval
 
-        if self.lang.lower() == 'go':
-            cmd = ['go run', self.filename]
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            err_code = p.wait()
-            retval = p.stdout.read().decode('utf8')
-            errmess = p.stderr.read().decode('utf8')
+		if self.lang.lower() == 'go':
+			cmd = ['go run', self.filename]
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			err_code = p.wait()
+			retval = p.stdout.read().decode('utf8')
+			errmess = p.stderr.read().decode('utf8')
 
-            if err_code:
-                print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
-            else:
-                print(colored('[INFO]', 'green'), "running finished ", retval)
-                return retval
+			if err_code:
+				print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
+			else:
+				print(colored('[INFO]', 'green'), "running finished ", retval)
+				return retval
 
-        if self.lang.lower() == 'c':
-            cmd = ['gcc', self.filename, '-o', 'start', '&', './start']
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            err_code = p.wait()
-            retval = p.stdout.read().decode('utf8')
-            errmess = p.stderr.read().decode('utf8')
+		if self.lang.lower() == 'c':
+			cmd = ['gcc', self.filename, '-o', 'start', '&', './start']
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			err_code = p.wait()
+			retval = p.stdout.read().decode('utf8')
+			errmess = p.stderr.read().decode('utf8')
 
-            if err_code:
-                print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
-            else:
-                print(colored('[INFO]', 'green'), "running finished ", retval)
-                return retval
+			if err_code:
+				print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
+			else:
+				print(colored('[INFO]', 'green'), "running finished ", retval)
+				return retval
 
-        if self.lang.lower() == 'cpp':
-            cmd = ['g++', self.filename, '-o', 'start', '&', './start']
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            err_code = p.wait()
-            # retval = p.stdout.read().decode('utf8')
-            retval = p.stdout.read()
-            errmess = p.stderr.read()
+		if self.lang.lower() == 'cpp':
+			cmd = ['g++', self.filename, '-o', 'start', '&', './start']
+			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			err_code = p.wait()
+			# retval = p.stdout.read().decode('utf8')
+			retval = p.stdout.read()
+			errmess = p.stderr.read()
 
-            if err_code:
-                print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
-            else:
-                print(colored('[INFO]', 'green'), "running finished ", retval)
-                return retval
+			if err_code:
+				print(colored('[INFO]', 'red'), 'error occured, error code: {0}, error message: {1}'.format(err_code, errmess))
+			else:
+				print(colored('[INFO]', 'green'), "running finished ", retval)
+				return retval
 
 if __name__ == '__main__':
-    limit = {
-        'time': None,
-        'mem': None
-    }
-    lang = 'cpp'
-    code = '''#include <iostream>\nusing namespace std;\n
+	limit = {
+		'time': None,
+		'mem': None
+	}
+	lang = 'cpp'
+	code = '''#include <iostream>\nusing namespace std;\n
 int main(){\ncout << "Hello, world" << endl;\nreturn 0;\n}'''
-    r = Runner(lang, code, limit)
-    r.run()
+	r = Runner(lang, code, limit)
+	r.run()
--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import socket
 
 
 class JudgeHoster:
-    
-    '''
-    using a port of tcp to get data transmission and put this in docker, that will be relatively safe
-    '''
-    def __init__(self, host, port):
-        self.max_thread = 100
-        self.judgeServer = socket.socket((host, port))
-    
+	
+	'''
+	using a port of tcp to get data transmission and put this in docker, that will be relatively safe
+	'''
+	def __init__(self, host, port):
+		self.max_thread = 100
+		self.judgeServer = socket.socket((host, port))
+	
 
-    def run(self):
-        while True:
-            pass
+	def run(self):
+		while True:
+			pass
 
-    def send(self, msg):
-        pass
+	def send(self, msg):
+		pass
 
-    def receive(self, msg):
-        pass
+	def receive(self, msg):
+		pass
 
-    def parseCmd(self, cmd):
-        pass
+	def parseCmd(self, cmd):
+		pass
 
-    def writeCmd(self, cmd):
-        pass
+	def writeCmd(self, cmd):
+		pass
