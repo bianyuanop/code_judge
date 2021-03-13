f79dd905e40993de097d2c4ebcd749c5931726ca
bianyuanop <chen.me.nan@foxmail.com>
2021-03-13 18:42:35 +0800

    Runner functionality well

--- lib/runner.py
+++ lib/runner.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import threading
 from termcolor import colored
 
 
@@ -13,18 +14,22 @@ Errors = {
 if not os.path.exists('tmp'):
 	os.mkdir("tmp")
 
-class Runner:
+class Runner(threading.Thread):
 	'''
 	a code runner 
 	'''
 	
-	def __init__(self, lang, code, limit, subId=0):
+	def __init__(self, lang, code, p_in, limit, subId=0):
 		'''
 		limit parse
 			- time
 			- mem
 		'''
+		threading.Thread.__init__(Runner)
+
 		self.prefix = 'tmp/'
+		self.result = {}
+		self.p_in = p_in
 		if lang == 'cpp':
 			self.filename = 'temp' + str(subId) + '.cpp'
 		elif lang == 'python':
@@ -33,8 +38,6 @@ class Runner:
 			self.filename = 'temp' + str(subId) + '.go'
 		elif lang == 'c':
 			self.filename = 'temp' + str(subId) + '.c'
-		else:
-			self.filename = "temp" + str(subId)
 
 		with open(self.prefix + self.filename, 'w') as f:
 			f.write(code)
@@ -42,6 +45,25 @@ class Runner:
 		self.lang = lang
 		self.timeLimit = limit['time']
 		self.memLimit = limit['mem']
+
+	def getResult(self):
+		'''This function must be called after join()'''
+		return self.result
+	
+	def sub(self, cmd, p_in=None):
+		p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		if p_in:
+			out, err = p.communicate(input=p_in, timeout=self.timeLimit)
+			if err:
+				return 1, out, err
+			else:
+				return 0, out, err
+
+		err_code = p.wait()
+		retval = p.stdout.read().decode('utf8')
+		errmess = p.stderr.read().decode('utf8')
+
+		return err_code, retval, errmess
 
 	def run(self):
 		'''
@@ -53,36 +75,39 @@ class Runner:
 			retval = p.stdout.read()
 			errmess = p.stderr.read()
 		'''
-		if self.lang.lower() == 'python':
-			cmd = ['python', self.prefix + self.filename]
-			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			err_code = p.wait()
-			retval = p.stdout.read().decode('utf8')
-			errmess = p.stderr.read().decode('utf8')
+	
+		try:
 
-		if self.lang.lower() == 'go':
-			cmd = ['go run ' + self.prefix + self.filename]
-			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			err_code = p.wait()
-			retval = p.stdout.read().decode('utf8')
-			errmess = p.stderr.read().decode('utf8')
+			if self.lang.lower() == 'python':
+				cmd = ['python', self.prefix + self.filename]
+				err_code, retval, errmess = self.sub(cmd, self.p_in)
+			elif self.lang.lower() == 'go':
+				cmd = ['go', 'build', self.prefix + self.filename]
+				err_code, retval, errmess = self.sub(cmd, self.p_in)
+				if not err_code:
+					cmd = ['./' + self.filename.split('.')[0]]
+					err_code, retval, errmess = self.sub(cmd, self.p_in)
 
-		if self.lang.lower() == 'c':
-			cmd = ['gcc', self.prefix + self.filename, '-o', 'start', '&&', './start']
-			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			err_code = p.wait()
-			retval = p.stdout.read().decode('utf8')
-			errmess = p.stderr.read().decode('utf8')
+			elif self.lang.lower() == 'cpp':
+				cmd = ['g++', self.prefix + self.filename, '-o', self.filename.split('.')[0]]
+				err_code, retval, errmess = self.sub(cmd, self.p_in)
+				if not err_code:
+					cmd = ['./' + self.filename.split('.')[0]]
+					err_code, retval, errmess = self.sub(cmd, self.p_in)
+					
+			elif self.lang.lower() == 'c':
+				cmd = ['gcc' , self.prefix + self.filename , '-o' , self.filename.split('.')[0]]
+				err_code, retval, errmess = self.sub(cmd, self.p_in)
+				if not err_code:
+					cmd = ['./' + self.filename.split('.')[0]]
+					err_code, retval, errmess = self.sub(cmd, self.p_in)
+		except Exception as e:
+			print(e)
+			return
 
-		if self.lang.lower() == 'cpp':
-			cmd = ['g++ ' + self.prefix + self.filename + ' -o' + ' start ' + '&&' + ' ./start']
-			p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			err_code = p.wait()
-			# retval = p.stdout.read().decode('utf8')
-			retval = p.stdout.read().decode('utf8')
-			errmess = p.stderr.read().decode('utf8')
-
-		return err_code, retval, errmess
+		self.result['err_code'] = err_code
+		self.result['retval'] = retval
+		self.result['errmess'] = errmess
 
 
 if __name__ == '__main__':
@@ -90,12 +115,17 @@ if __name__ == '__main__':
 		'time': None,
 		'mem': None
 	}
-	lang = 'cpp'
-	code = '''#include <iostream>
-using namespace std;
+	lang = 'c'
+	code = r'''#include <stdio.h>
 int main() {
-	cout << "Hello, world" << endl;
-	return 0;	
+	char name[80];
+	scanf("%s", name);
+	printf("%s\n", name);
+	return 0;;
 }'''
-	r = Runner(lang, code, limit)
-	r.run()
+	p_in = "FUCK you son of bitch".encode('utf8')
+	r = Runner(lang, code, p_in, limit)
+	r.start()
+	r.join()
+	result = r.getResult()
+	print(result)
